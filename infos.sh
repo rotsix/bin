879ac95ecada6b2f88244e6648c93f6c95fae07b
Victor Franzi <victor.franzi@gmail.com>
2019-02-18 00:44:44 +0100

    display current song

--- infos.sh
+++ infos.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-notify-send "$(date '+%B %d %Y - %R')" "Battery:$(acpi -V | head -n 1 | cut -d ',' -f 2)"
+current="$(mpc current 2> /dev/null)"
+
+notify-send "$(date '+%B %d %Y - %R')" "Battery:$(acpi -V | head -n 1 | cut -d ',' -f 2)\\n$current" --icon=dialog-information
