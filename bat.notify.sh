9af9dee291859cf8769e01d25f2f52b5a89b9610
Victor Franzi <victor.franzi@gmail.com>
2019-02-23 21:34:20 +0100

    lower case

--- bat.notify.sh
+++ bat.notify.sh
@@ -4,7 +4,7 @@ while true; do
 	actual="$(cat /sys/class/power_supply/BAT0/capacity)"
 	if [ "$actual" -le "25" ] && [ "$(cat /sys/class/power_supply/BAT0/status)" = "Discharging" ]
 	then
-		notify-send --urgency=critical "Low battery!" "Battery: $actual%"
+		notify-send --urgency=critical "low battery" "battery: $actual%"
 	fi
 
 	sleep 2m
