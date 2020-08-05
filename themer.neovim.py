ba23b54bc8fd7e5df01a5a9132dcd3d9a87525f8
Victor Franzi <victor.franzi@gmail.com>
2020-08-05 20:48:51 +0200

    themer: fix this python mess

--- themer.neovim.py
+++ themer.neovim.py
@@ -3,14 +3,12 @@
 # rotsix - (c) wtfpl 2017
 # reload all neovim sessions
 
-import os
 from neovim import attach
 from glob import glob
 
-try:
-    neovimInstances = glob("/tmp/nvim*/0")
-    for p in neovimInstances:
+for p in glob("/tmp/nvim*/0"):
+    try:
         nvim = attach("socket", path=p)
         nvim.command("source ~/.config/nvim/init.vim")
-except ConnectionRefusedError:
-    pass
+    except ConnectionRefusedError:
+        pass
