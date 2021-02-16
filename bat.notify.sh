bd12e4dbbc44d57caeed44847a142f4faf1bacb3
Victor Franzi <victor.franzi@gmail.com>
2021-02-16 20:01:56 +0100

    bat.notify: add a notification on half battery

--- bat.notify.sh
+++ bat.notify.sh
@@ -2,6 +2,11 @@
 
 while true; do
 	actual="$(cat /sys/class/power_supply/BAT0/capacity)"
+	if [ "$actual" -eq "50" ]
+	then
+		notify-send --urgency=critical "medium battery" "battery: $actual%"
+	fi
+
 	if [ "$actual" -le "25" ] && [ "$(cat /sys/class/power_supply/BAT0/status)" = "Discharging" ]
 	then
 		notify-send --urgency=critical "low battery" "battery: $actual%"
